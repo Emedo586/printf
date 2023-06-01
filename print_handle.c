5755015125c786ffff6b8653f6354b696004d10b
Emedo586 <emedopeace99@gmail.com>
2023-05-31 18:35:42 -0700

    print_handle.c

--- print_handle.c
+++ print_handle.c
@@ -14,8 +14,7 @@
  */
 
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-          int flags, int width, int precision, int size)
-
+int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -25,11 +24,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-
-        for (i = 0; fmt_types[i].fmt != '\0'; i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-	
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
@@ -41,9 +38,9 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-			       	--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
+			--(*ind);
+		if (fmt[*ind] == ' ')
+		--(*ind);
 			return (1);
 		}
 		unknow_len += write(1, &fmt[*ind], 1);
